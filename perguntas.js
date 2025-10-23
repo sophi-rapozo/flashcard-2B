2055e64c33051b29b061aa653667258b506d921e
sophi-rapozo <sophia.rapozo@escola.pr.gov.br>
2025-10-23 07:41:48 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,34 @@
+criaCartao(
+    'Escultura'
+    'Qual material era mais usado pelos escultores gregos antigos'
+    'Mármore'
+)
 
+criaCartao(
+    'Mitologia grega'
+    'Quem era a deusa do amor?'
+    'Afrodite'
+)
+
+criaCartao(
+    'Criaturas gregas'
+    'Quem era a criatura que tinha serpentes na cabeça e transformava as pessoas em pedra?'
+    'Medusa'
+)
+
+criaCartao(
+    'Reinos grgos'
+    'Qual deus governa o submundo?'
+    'Hades'
+)
+
+criaCartao(
+    'Mitologia grega'
+    'Quem era o deus da luz?'
+    'Apolo'
+)
+
+criaCartao(
+    'Heroi da guerra cuja unica fraqueza era o calcanhar'
+    'Aquiles'
+)
